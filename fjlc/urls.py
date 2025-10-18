1b624ac680fb61b6fbff33f5589454ba99c6fa8c
Lewis Irakoze <lewisirakoz@gmail.com>
2025-10-18 11:51:06 +0200

    changed project's urls.py set /admin open django admin not from frontend

--- fjlc/urls.py
+++ fjlc/urls.py
@@ -6,18 +6,20 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
+
+    # API endpoints
     path("api/", include("job.urls")),
     path("api/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
-# Serve media files during development
+# Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# Catch-all for React frontend (only if not starting with api/ or admin/)
 urlpatterns += [
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+    re_path(r"^(?!admin)(?!api).*", TemplateView.as_view(template_name="index.html")),
 ]
-
-
